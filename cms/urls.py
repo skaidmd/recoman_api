097ee2e0a95c678cb2ad30757f771a421094fc59
skai <skaidmd@gmail.com>
2018-12-01 14:15:19 +0900

    既存ロジックに渡すため、POSTの内容を取得し、リスト化

--- cms/urls.py
+++ cms/urls.py
@@ -5,9 +5,6 @@ from cms import views
 app_name = 'cms'
 urlpatterns = [
 
-    path('recoman_top/', views.recoman_top, name='recoman_top'),   # top
-    path('recoman_top/search', views.recoman_search, name='recoman_search'),  # search
-    path('recoman_top/searchresult', views.recoman_searchresult, name='recoman_searchresult'),  # searchresult
-    path('recoman_top/history', views.recoman_history, name='recoman_history'),  # history
     path('api/analyze', views.analyze, name='analyze'),  # analyze
+#    path('api/debug', views.debug, name='debug'),  # debug
 ]
--- cms/views.py
+++ cms/views.py
@@ -2,13 +2,17 @@ import json
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from .forms import select_manga
 import numpy as np
 
 import sys
+
 sys.path.append("/Users/skai/PycharmProjects/recoman/cms")
 
-import boklog_content_v2,title_cleansing
+import boklog_content_v2, title_cleansing
+
 
 def render_json_response(request, data, status=None):
     """response を JSON で返却"""
@@ -23,87 +27,81 @@ def render_json_response(request, data, status=None):
         response = HttpResponse(json_str, content_type='application/json; charset=UTF-8', status=status)
     return response
 
+
+# @csrf_exempt
+# def debug(request):
+#     data = request.POST
+#     values = json.loads(request.body)
+#     # print(values["name"])
+#     # print(values["series"])
+#
+#     lovetitlel=[]
+#
+#     for i in values["series"]:
+#         lovetitlel.append([values["name"],i,values["series"][i]])
+#
+#     print(lovetitlel)
+#
+#     return render_json_response(request, lovetitlel)
+
+
+@csrf_exempt
 def analyze(request):
     """analyze"""
-    data = {'進撃': [1,2,3], 'k2': 2, 'k3': 3}
-    return render_json_response(request, data)
+    # 受け取ったPOSTデータ
+    values = json.loads(request.body)
 
+    lovetitles = []
 
-def recoman_top(request):
-    """recoman_top"""
-    username = 'testuser'
+    for i in values["series"]:
+        lovetitles.append([values["name"], i, values["series"][i]])
 
-    return render(request,'cms/recoman_top.html',{'username':username})
+    # lovetitlel = [
+    #     ['kensusan', 'G戦場ヘヴンズドア', 5],
+    #     ['kensusan', '沈黙の艦隊', 4],
+    #     ['kensusan', 'からかい上手の高木さん', 4],
+    #     ['kensusan', '進撃の巨人', 5],
+    #     ['kensusan', 'HUNTER X HUNTER', 5],
+    #     ['kensusan', 'カジュアル面談大好きっ子', 5]
+    # ]
 
-def recoman_search(request):
-    """recoman_search"""
-    form = select_manga()
+    # データ追加
+    # 選択データDB格納
 
-    # ページ到達時点でjsonデータ読み込み、データ化
-    # ページ到達に関わらないアプローチに変えたい
-    # boklog_content_v2.load_log('dummy')
+    username = lovetitles[0][0]
 
-    return render(request,
-                    'cms/recoman_search.html',
-                    {'form': form})
+    try:
+        df_Alllog = boklog_content_v2.target_add(lovetitles)
+        # タイトル情報からシリーズ情報を加工
+        # ユーザーシリーズデータの加工
+        df_Alllog['series'] = title_cleansing.series_ext(df_Alllog['title'])
 
-def recoman_searchresult(request):
-    """recoman_searchsult"""
+        # ログをグループ化して集計
+        df_logcust = df_Alllog.groupby(['booklogUserId', 'series'], as_index=False).agg(
+            {'reviewScore': [np.size, np.max, np.min, np.mean]})
+        df_logcust.columns = ['booklogUserId', 'series', 'rv_num', 'rv_max', 'rv_min', 'rv_mean']
 
-    #受け取ったPOSTデータ
-    form = select_manga(data=request.POST)
+        # マスタデータピボット化
+        df_pivot = df_logcust.pivot(index='booklogUserId', columns='series', values='rv_max').fillna(0)
+        df_pivot = df_pivot.reset_index(drop=False)
 
-    username = form['username'].value()
+        # 評価
+        rank = boklog_content_v2.recomend(username, 20, df_pivot)
 
-    lovetitlel=[
-        [username, form['manga1'].value(),form['evaluat1'].value()],
-        [username, form['manga2'].value(),form['evaluat2'].value()],
-        [username, form['manga3'].value(),form['evaluat3'].value()],
-        [username, form['manga4'].value(),form['evaluat4'].value()],
-        [username, form['manga5'].value(),form['evaluat5'].value()]
-    ]
+        rankl = []
+        num = 20
+        cnt = 0
+        for i, j in rank:
+            if j > 0:
+                if cnt < num:
+                    rankl.append([df_pivot.iloc[0:0, i + 1:i + 2].columns.values[0], round(j, 3)])
+                    cnt += 1
+            else:
+                continue
 
-    #データ追加
-    #選択データDB格納
+        rank_dic = dict(rankl)
 
-    df_Alllog = boklog_content_v2.target_add(lovetitlel)
+        return render_json_response(request, rank_dic)
 
-    # タイトル情報からシリーズ情報を加工
-    # ユーザーシリーズデータの加工
-    df_Alllog['series'] = title_cleansing.series_ext(df_Alllog['title'])
-
-    # ログをグループ化して集計
-    df_logcust = df_Alllog.groupby(['booklogUserId', 'series'], as_index=False).agg({'reviewScore': [np.size, np.max, np.min, np.mean]})
-    df_logcust.columns = ['booklogUserId', 'series', 'rv_num', 'rv_max', 'rv_min', 'rv_mean']
-
-    # マスタデータピボット化
-    # データは別保存でうまいことできなかなー
-    # （通常）ピボット化
-    df_pivot = df_logcust.pivot(index='booklogUserId', columns='series', values='rv_max').fillna(0)
-    df_pivot = df_pivot.reset_index(drop=False)
-
-    #評価
-    rank=boklog_content_v2.recomend(username,20,df_pivot)
-
-    rankl=[]
-    num=20
-    cnt=0
-    for i,j in rank:
-        if j>0:
-            if cnt <num:
-                rankl.append([df_pivot.iloc[0:0,i+1:i+2].columns.values[0],',',round(j,3)])
-                cnt+=1
-        else:
-            continue
-
-
-    return render(request,
-                'cms/recoman_searchresult.html',
-                {'rankl': rankl})
-
-
-
-def recoman_history(request):
-    """recoman_history"""
-    return render(request,'cms/recoman_history.html')
-
+    except:
+        return render_json_response(request, "なんらかのエラーが発生しました")
